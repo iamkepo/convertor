0043d8a84fb47570c4367255756ae5bf5980cd86
christamour.kakpo <christamour.kakpo@gmail.com>
2022-12-08 13:01:34 +0100

    first commit

---
+++ javascripts/app.js
@@ -0,0 +1,33 @@
+function convert () {
+  let result = select('.text-input').value.split('\n').map((item, i)=> (
+    {
+      name: nameDestructuring(item),
+      number: parseInt(select('.number-input').value)+i,
+    }
+  ))
+  write(result)
+}
+function nameDestructuring(param) {
+  if(param.split('.').length > 1)  {
+    return param.split('').map((item, i)=> ( (i > param.split('.')[0].length) ? item : '' )).join(',').replaceAll(',', '')
+  } else {
+    return param
+  }
+}
+function write(list) {
+  let html_code = "<div class='m-1'>pollingStations: [</div>";
+  list.forEach(item => {
+    html_code = html_code.concat(`
+      <div class='m-2'>{</div>
+      <div class='m-3'>name: "${item.name}",</div>
+      <div class='m-3'>number: ${item.number},</div>
+      <div class='m-2'>},</div>
+    `)
+  });
+  
+  html_code = html_code.concat("<div class='m-1'>]</div>");
+  select('.text-output').innerHTML = html_code;
+}
+function select(param) {
+  document.querySelector(param)
+}
